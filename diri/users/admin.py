08ed16d9943dfd31bd8159a9b09ec5004c133870
Jeremiah David <jeremiahedavid@gmail.com>
2021-03-16 04:24:14 +0100

    added csv export function to admin

--- diri/users/admin.py
+++ diri/users/admin.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
@@ -6,6 +8,24 @@ from .models import Entrepreneurs
 from diri.users.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected models as CSV format"
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
@@ -33,4 +53,29 @@ class UserAdmin(auth_admin.UserAdmin):
     search_fields = ["name"]
 
 
-admin.site.register(Entrepreneurs)
+
+
+@admin.register(Entrepreneurs)
+class EntrepreneursAdmin(admin.ModelAdmin, ExportCsvMixin):
+    list_display = [
+        "amount",
+        "first_name",
+        "mid_name",
+        "last_name",
+        "email",
+        "phone",
+        "state",
+        "lga",
+        "bvn",
+        "bank_name",
+        "acc_no",
+        "st_o_acc",
+    ]
+    list_editable = [
+        "first_name",
+        "mid_name",
+        "last_name",
+        "email",
+        "phone",
+    ]
+    actions = ["export_as_csv"]
